b4574da95b6d679efcbb2209e1faacf36833ce34
Jelle Claes <jelleclaes0@gmail.com>
2023-04-28 01:26:52 +0200

    merged UDP and TCP server and clients together in server.c and client.c

--- client.c
+++ client.c
@@ -38,12 +38,27 @@ void OSCleanup( void )
 
 #include <stdint.h>
 #include <time.h>
+#include <math.h>
 
 int initializationUDP( struct sockaddr ** internet_address, socklen_t * internet_address_length );
 void executionUDP( int internet_socket, struct sockaddr * internet_address, socklen_t internet_address_length );
 void cleanupUDP( int internet_socket, struct sockaddr * internet_address );
 
+int initializationTCP();
+void executionTCP( int internet_socket );
+void cleanupTCP( int internet_socket );
+uint16_t randomNumberTCP();
+char randomOperationTCP();
+void checkAnswerTCP(char buffer[100],float number1,float number2,char operation);
+
 int main(int argc, char *argv[]) {
+
+    srand(time(NULL));
+
+    //START UDP
+    printf("Start UDP client\n");
+    printf("\n");
+
     OSInit();
 
 
@@ -65,6 +80,24 @@ int main(int argc, char *argv[]) {
     cleanupUDP(internet_socketUDP, internet_address);
 
     OSCleanup();
+    //END UDP
+
+    //START TCP
+    printf("\n");
+    printf("Start TCP client\n");
+    printf("\n");
+    OSInit();
+
+    int internet_socketTCP = initializationTCP();
+
+    executionTCP( internet_socketTCP );
+
+    cleanupTCP( internet_socketTCP );
+
+    OSCleanup();
+    //END TCP
+    printf("\n");
+    printf("all ended well\n");
 
     return 0;
 }
@@ -202,3 +235,211 @@ void cleanupUDP( int internet_socket, struct sockaddr * internet_address )
     //Step 3.1
     close( internet_socket );
 }
+
+int initializationTCP()
+{
+    //Step 1.1
+    struct addrinfo internet_address_setup;
+    struct addrinfo * internet_address_result;
+    memset( &internet_address_setup, 0, sizeof internet_address_setup );
+    internet_address_setup.ai_family = AF_UNSPEC;
+    internet_address_setup.ai_socktype = SOCK_STREAM;
+    int getaddrinfo_return = getaddrinfo( "::1", "24042", &internet_address_setup, &internet_address_result );
+    if( getaddrinfo_return != 0 )
+    {
+        fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
+        exit( 1 );
+    }
+
+    int internet_socket = -1;
+    struct addrinfo * internet_address_result_iterator = internet_address_result;
+    while( internet_address_result_iterator != NULL )
+    {
+        //Step 1.2
+        internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
+        if( internet_socket == -1 )
+        {
+            perror( "socket" );
+        }
+        else
+        {
+            //Step 1.3
+            int connect_return = connect( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
+            if( connect_return == -1 )
+            {
+                perror( "connect" );
+                close( internet_socket );
+            }
+            else
+            {
+                break;
+            }
+        }
+        internet_address_result_iterator = internet_address_result_iterator->ai_next;
+    }
+
+    freeaddrinfo( internet_address_result );
+
+    if( internet_socket == -1 )
+    {
+        fprintf( stderr, "socket: no valid socket address found\n" );
+        exit( 2 );
+    }
+
+    return internet_socket;
+}
+
+void executionTCP( int internet_socket )
+{
+    uint16_t number1 = 0;
+    uint16_t number2 = 0;
+    char operation = '0';
+    char sendOperation[15];
+    int number_of_bytes_send = 0;
+    int number_of_bytes_received = 0;
+    char buffer[1000];
+    int amountOfOperations = 0;
+
+    amountOfOperations = (rand() % 20) +1;
+    printf("amountOfOperations = %d\n",amountOfOperations);
+
+    for(int i=0;i<amountOfOperations;i++){
+
+        number1 = randomNumberTCP();
+        number2 = randomNumberTCP();
+        operation = randomOperationTCP();
+
+        sprintf(sendOperation,"%d %c %d",number1,operation,number2);
+
+
+        number_of_bytes_send = send( internet_socket, (const char*)sendOperation, strlen(sendOperation), 0 );
+        if( number_of_bytes_send == -1 )
+        {
+            perror( "send" );
+        }
+
+
+
+        number_of_bytes_received = recv( internet_socket, buffer, ( sizeof buffer ) - 1, 0 );
+        if( number_of_bytes_received == -1 )
+        {
+            perror( "recv" );
+        }
+        else
+        {
+            buffer[number_of_bytes_received] = '\0';
+            printf( "Received : %s\n", buffer );
+        }
+
+        checkAnswerTCP(buffer,number1,number2,operation);
+    }
+
+
+    number_of_bytes_send = send( internet_socket,"STOP\n", strlen("STOP\n"), 0 );
+    if( number_of_bytes_send == -1 )
+    {
+        perror( "send" );
+    }
+
+    number_of_bytes_received = recv( internet_socket, buffer, ( sizeof buffer ) - 1, 0 );
+    if( number_of_bytes_received == -1 )
+    {
+        perror( "recv" );
+    }
+    else
+    {
+        buffer[number_of_bytes_received] = '\0';
+        printf( "Received : %s\n", buffer );
+    }
+    if(strcmp(buffer,"OK\n")==0){
+        number_of_bytes_send = send( internet_socket,"KTNXBYE\n", strlen("KTNXBYE\n"), 0 );
+        if( number_of_bytes_send == -1 )
+        {
+            perror( "send" );
+        }
+    }
+
+
+    //Step 2.1
+
+
+    //Step 2.2
+
+}
+
+void cleanupTCP( int internet_socket )
+{
+    //Step 3.2
+    int shutdown_return = shutdown( internet_socket, SD_SEND );
+    if( shutdown_return == -1 )
+    {
+        perror( "shutdown" );
+    }
+
+    //Step 3.1
+    close( internet_socket );
+}
+
+uint16_t randomNumberTCP(){
+    //generate a random number between 0 and 99
+    uint16_t randomNumber = 0;
+    randomNumber = rand() % 100;
+    return randomNumber;
+}
+
+char randomOperationTCP(){
+    uint16_t randomNumberOperation = rand() % 4;
+    char randomOperation = '0';
+    switch (randomNumberOperation) {
+        case 0:
+            randomOperation = '+';
+            break;
+        case 1:
+            randomOperation = '-';
+            break;
+        case 2:
+            randomOperation = '*';
+            break;
+        case 3:
+            randomOperation = '/';
+            break;
+    }
+    return randomOperation;
+}
+
+void checkAnswerTCP(char buffer[100],float number1,float number2,char operation){
+    float result=0;
+    float expectedVal = 0;
+
+    switch (operation) {
+        case '+':
+            expectedVal = number1 + number2;
+            break;
+        case '-':
+            expectedVal = number1 - number2;
+            break;
+        case '*':
+            expectedVal = number1 * number2;
+            break;
+        case '/':
+            if(number2 == 0){
+                printf("Division By 0\n");;
+            }
+            else {
+                expectedVal = number1 / (float)number2;
+                expectedVal = floorf(expectedVal *100 ) / 100; //round down to 2 decimals
+            }
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+    result = atof(buffer);
+    printf("expected val = %f\n",expectedVal);
+    printf("received result = %f\n",result);
+    if(result == expectedVal){
+        printf("correct\n");
+    } else {
+        printf("incorrect\n");
+    }
+}
+
--- server.c
+++ server.c
@@ -46,12 +46,19 @@ int randomNumberUDP();
 void sendNumberUDP(int internet_socket,struct sockaddr_storage client_internet_address,socklen_t client_internet_address_length,int number_of_bytes_send);
 void receiveNumberUDP(char buffer[1000],int internet_socket,struct sockaddr_storage client_internet_address,socklen_t client_internet_address_length,int number_of_bytes_received);
 
+int initializationTCP();
+int connectionTCP( int internet_socket );
+void executionTCP( int internet_socket );
+void cleanupTCP( int internet_socket, int client_internet_socket );
+
 
 int main( int argc, char * argv[] ) {
-    //UDP
 
     srand(time(NULL));
 
+    //START UDP
+    printf("Start UDP server\n");
+    printf("\n");
     //initialize the socket and the OS
     OSInit();
 
@@ -75,6 +82,26 @@ int main( int argc, char * argv[] ) {
     OSCleanup();
 
     //END UDP
+
+    //START TCP
+    printf("\n");
+    printf("Start TCP server\n");
+    printf("\n");
+    OSInit();
+
+    int internet_socketTCP = initializationTCP();
+
+    int client_internet_socketTCP = connectionTCP( internet_socketTCP );
+
+    executionTCP( client_internet_socketTCP );
+
+    cleanupTCP( internet_socketTCP, client_internet_socketTCP );
+
+    OSCleanup();
+    //END TCP
+
+    printf("\n");
+    printf("all ended well\n");
 
     return 0;
 }
@@ -217,4 +244,204 @@ void receiveNumberUDP(char buffer[1000],int internet_socket,struct sockaddr_stor
         buffer[number_of_bytes_received] = '\0';
         printf( "Received : %hd\n", ntohs(*((short*)buffer)) );
     }
+}
+
+int initializationTCP()
+{
+    //Step 1.1
+    struct addrinfo internet_address_setup;
+    struct addrinfo * internet_address_result;
+    memset( &internet_address_setup, 0, sizeof internet_address_setup );
+    internet_address_setup.ai_family = AF_UNSPEC;
+    internet_address_setup.ai_socktype = SOCK_STREAM;
+    internet_address_setup.ai_flags = AI_PASSIVE;
+    int getaddrinfo_return = getaddrinfo( NULL, "24042", &internet_address_setup, &internet_address_result );
+    if( getaddrinfo_return != 0 )
+    {
+        fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
+        exit( 1 );
+    }
+
+    int internet_socket = -1;
+    struct addrinfo * internet_address_result_iterator = internet_address_result;
+    while( internet_address_result_iterator != NULL )
+    {
+        //Step 1.2
+        internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
+        if( internet_socket == -1 )
+        {
+            perror( "socket" );
+        }
+        else
+        {
+            //Step 1.3
+            int bind_return = bind( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
+            if( bind_return == -1 )
+            {
+                perror( "bind" );
+                close( internet_socket );
+            }
+            else
+            {
+                //Step 1.4
+                int listen_return = listen( internet_socket, 1 );
+                if( listen_return == -1 )
+                {
+                    close( internet_socket );
+                    perror( "listen" );
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        internet_address_result_iterator = internet_address_result_iterator->ai_next;
+    }
+
+    freeaddrinfo( internet_address_result );
+
+    if( internet_socket == -1 )
+    {
+        fprintf( stderr, "socket: no valid socket address found\n" );
+        exit( 2 );
+    }
+
+    return internet_socket;
+}
+
+int connectionTCP( int internet_socket )
+{
+    //Step 2.1
+    struct sockaddr_storage client_internet_address;
+    socklen_t client_internet_address_length = sizeof client_internet_address;
+    int client_socket = accept( internet_socket, (struct sockaddr *) &client_internet_address, &client_internet_address_length );
+    if( client_socket == -1 )
+    {
+        perror( "accept" );
+        close( internet_socket );
+        exit( 3 );
+    }
+    return client_socket;
+}
+
+void executionTCP(int internet_socket) {
+    char buffer[1000];
+    int number_of_bytes_received;
+    while (1) {
+        // Step 3.1
+        memset(buffer, 0, sizeof(buffer));
+        number_of_bytes_received = 0;
+        //char buffer[1000];
+        number_of_bytes_received = recv(internet_socket, buffer, (sizeof buffer) - 1, 0);
+        if (number_of_bytes_received == -1) {
+            perror("recv");
+            break;
+        } else if (number_of_bytes_received == 0) {
+            printf("Client disconnected\n");
+            break;
+        }
+
+        buffer[number_of_bytes_received] = '\0';
+        if (strcmp(buffer, "STOP\n") == 0) {
+            printf("Received Stop\n");
+            send(internet_socket, "OK\n", strlen("OK\n"), 0);
+
+            number_of_bytes_received = recv(internet_socket, buffer, (sizeof buffer) -1 , 0);
+            if(number_of_bytes_received == -1){
+                perror("recv");
+                break;
+            }
+            buffer[number_of_bytes_received] = '\0';
+            if(strcmp(buffer, "KTNXBYE\n") == 0){
+                printf("received KTNXBYE\n");
+                break;
+            }
+        }
+        printf("Received message: %s\n", buffer);
+        //send(internet_socket, buffer, strlen(buffer), 0);
+        // }
+
+        /*while (1){
+            memset(buffer, 0 , sizeof(buffer));
+            number_of_bytes_received = recv(internet_socket, buffer, (sizeof buffer) -1 , 0);
+            if(number_of_bytes_received == -1){
+                perror("recv");
+                break;
+            }
+            else if(number_of_bytes_received == 0){
+                printf("Client disconnected\n");
+                break;
+            }
+            buffer[number_of_bytes_received] = '\0';
+            if(strcmp(buffer, "KTNXBYE\n") == 0){
+                printf("received KTNXBYE\n");
+                break;
+            }*/
+
+        // Step 3.2: parse the operation
+        int num1, num2;
+        char op;
+        if (sscanf(buffer, "%d %c %d", &num1, &op, &num2) != 3) {
+            printf("Invalid operation format\n");
+            continue;
+        }
+
+        // Step 3.3: perform the calculation
+        float result;
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if(num2 == 0){
+                    printf("Division By 0\n");
+                    int number_of_bytes_sent = send(internet_socket, "Division by 0\n", strlen("Division by 0\n"), 0);
+                    if (number_of_bytes_sent == -1) {
+                        perror("send");
+                        break;
+                    }
+                }
+                else {
+                    result = num1 / (float) num2;
+                }
+                break;
+            default:
+                printf("Invalid operator\n");
+                continue;
+        }
+
+        // Step 3.4: send the result back to the client
+        char result_str[100];
+        snprintf(result_str, 5 , "%f" , result);
+        printf("result string= %s\n",result_str);
+        printf("result= %f\n",result);
+        int number_of_bytes_sent = send(internet_socket, result_str, strlen(result_str), 0);
+        if (number_of_bytes_sent == -1) {
+            perror("send");
+            break;
+        } else {
+            printf("Sent: %s\n", result_str);
+        }
+    }
+}
+
+void cleanupTCP( int internet_socket, int client_internet_socket )
+{
+    //Step 4.2
+    int shutdown_return = shutdown( client_internet_socket, SD_RECEIVE );
+    if( shutdown_return == -1 )
+    {
+        perror( "shutdown" );
+    }
+
+    //Step 4.1
+    close( client_internet_socket );
+    close( internet_socket );
 }
